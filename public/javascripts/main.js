c2faeb2827f326161e071dde0abdcb8a9d7cdff7
Lyn Nagara <lyn.nagara@gmail.com>
2015-04-10 14:24:21 -0400

    generates random short urls

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,32 +1,43 @@
 +(function() {
   var form = $('#submitForm');
   var urlText = $('#url');
+  var errorField = $('#error');
+  var successField = $('#success');
   var error;
 
   form.submit(function(evt) {
+    clearError();
     evt.preventDefault();
-    if (urlText.value === '') {
-      error = 'Please provide a value';
+    if (!urlText.val()) {
+      showError('Please provide a value');
     } else {
       $.ajax({
         type: 'POST',
         url: 'submit-link',
-        data: { test: 1},
+        data: { url: urlText.val()},
         dataType: 'json',
         timeout: 10000,
         success: function(data) {
           handleFormSuccess(data);
         },
         error: function(request, status, err) {
-          error = 'An error occurred';
+          showError('An error occurred');
         }
       });
     }
   });
 
   function handleFormSuccess(data) {
-    console.log('data');
-    
+    var str = 'Your short url is ' + window.location.protocol + '//' + window.location.host + '/' + data.slug;
+    successField.html(str);
+  }
+
+  function clearError() {
+    errorField.html('');
+  }
+
+  function showError(message) {
+    errorField.html(message);
   }
 
 })();
--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,35 @@
-
-/*
- * GET home page.
- */
-
 exports.index = function(req, res){
   res.render('index', { title: 'URL Shortener' });
 };
 
+var urls = [];
+
 exports.submitlink = function(req, res){
-  console.log('do stuff!')
-  // console.log(req);
-  // console.log(res);
-  // res.render('index', { title: 'URL Shortener' });
+  var url = req.body.url;
+  if (!url) { 
+    res.send(404);
+  };
+
+
+  var rand = Math.random().toString(36).substring(2,7);
+  urls.push({
+    'url': url,
+    'slug': rand
+  });
+  res.json({'slug': rand, 'url': url});
+};
+
+exports.redirectlink = function(req, res) {
+  var slug = req.route.params.slug;
+  var url = urls.filter(function(u) {
+    if (u.slug === 'slug') { return true };
+  })[0];
+
+  if (url === undefined) {
+    console.log('undefined')
+  } else {
+    console.log(url);
+  }
+
+  res.render('index', { title: 'URL Shortener' });
 };
